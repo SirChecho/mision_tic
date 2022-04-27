d2346b21ce5b28ea3ddea6a65ee2f48f0b78fac2
Sergio Mora <seres.mora@hotmail.com>
2022-04-26 22:41:59 -0500

    introduccion POO, clases, atributos, metodos, constructores, tipos de datos

---
+++ basic_java/Classes/src/main/java/com/mycompany/classes/Celular.java
@@ -0,0 +1,31 @@
+package com.mycompany.classes;
+
+public class Celular {
+
+//atributos
+    public String marca;
+    public int ram;
+    public int year;
+
+//constructor
+    public Celular(String brand, int ram, int year) {
+
+        this.marca = brand;
+        this.ram   = ram;
+        this.year  = year;
+
+    }
+
+//metodos 
+    public void encender() {
+        System.out.println("Encendiendo");
+    }
+    public void llamar(String nombre){
+        System.out.println("Llamando a " + nombre);
+    }
+    public String[] caracteristicas(){
+       String [] carac = 
+    
+    }
+
+}
